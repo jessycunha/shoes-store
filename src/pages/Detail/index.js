7ef13e5e4b4c7caaaf4409e4d77f5cd09023d3c6
Jessica Cunha <jessic4cunha@gmail.com>
2021-03-05 00:37:51 -0300

    new page

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,10 +1,101 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, ScrollView, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function Detail() {
- return (
-    <View>
-        <Text>Página Detail</Text>
-    </View>
+import Dot from '../../component/Dot';
+import SizeButton from '../../component/SizeButton';
+import Button from '../../component/Button';
+
+export default function Detail({ navigation }) {
+
+  navigation.setOptions({
+    headerTitle: 'Nike Air Jordan'
+  })
+
+  return (
+    <ScrollView style={styles.container}>
+        <Image
+          source={require('../../assets/detail.png')}
+          style={styles.image}
+          resizeMode="cover"
+        />
+
+        <View>
+          <View>
+            <Text style={[styles.title, {fontSize: 24} ]}>R$290,00</Text>
+          </View>
+          
+          <View opacity={0.4}>
+            <Text style={[styles.title, {fontSize: 30} ]}>Nike Air Max</Text>
+          </View>
+
+          <View style={styles.dotContainer}>
+            <Dot color="#2379F4"/>
+            <Dot color="#FB6E53"/>
+            <Dot color="#DDD"/>
+            <Dot color="#000"/>
+          </View>
+
+          <View style={{flexDirection: 'row', width: '100%'}}>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              <SizeButton bgColor="#17181A" color="#FFF">37</SizeButton>
+              <SizeButton>38</SizeButton>
+              <SizeButton>39</SizeButton>
+              <SizeButton>42</SizeButton>
+            </ScrollView>
+          </View>
+
+          <View style={styles.textContent}>
+            <Text style={styles.textContent}>
+              Nike Air Jordan
+            </Text>
+            <Text style={styles.textTitle}>
+              O tênis Nike Air Jordan traz conforto, amortecimento e suporte atualizados, para garantir uma passada estável e anatômica. Esse tênis é confeccionado em material respirável e cabedal em couro sintético.
+            </Text>
+            <Text style={styles.textList}>
+              - Categoria: Amortecimento
+            </Text>
+            <Text style={styles.textList}>
+              - Material: Mesh
+            </Text>
+          </View>
+
+          <View>
+            <Button/>
+          </View>
+        </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    width: '100%',
+    backgroundColor: '#FFF'
+  },
+  image:{
+    width: '100%'
+  },
+  title:{
+    fontFamily: 'Anton_400Regular',
+    paddingHorizontal: '2%'
+  },
+  dotContainer:{
+    flexDirection: 'row',
+    marginVertical: '7%'
+  },
+  textContent:{
+    fontSize: 16,
+    lineHeight: 25,
+    marginVertical: '2%',
+    paddingVertical: '2%'
+  },
+  textTitle:{
+    fontSize: 22,
+    marginVertical: '2%'
+  },
+  textList:{
+    fontSize: 16,
+    lineHeight:25
+  }
+});
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,97 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native'
+
+import Shoes from '../../component/Shoes';
 
 export default function Home() {
+
+ const navigation = useNavigation();
+
  return (
-    <View>
-        <Text style={{ marginTop: 60, fontSize: 25, fontFamily: 'Anton_400Regular' }}>Página Home</Text>
+    <View style={styles.container}>
+        <View style={styles.header}>
+          <Image
+            source={require('../../assets/banner.png')}
+            style={styles.image}
+          />
+
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>TÊNIS</Text>
+            <Text style={[styles.text, { color: '#CECECF' } ]}>•</Text>
+            <Text style={[styles.text, { color: '#CECECF' } ]}>CORRIDA</Text>
+            <TouchableOpacity style={{position: 'absolute', right: 0, alignSelf: 'center'}}>
+              <MaterialIcons
+                name="filter-list"
+                size={24}
+                color="#000"
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <View style={styles.line}/>
+
+        <ScrollView>
+          <Text style={styles.text}>LANÇAMENTOS</Text>
+
+          <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+            <Shoes img={require('../../assets/1.png')} cost="R$145,00">
+              Nike Air Max
+            </Shoes>
+            <Shoes img={require('../../assets/2.png')} cost="R$290,00" onClick={() => navigation.navigate('Detail')}>
+              Nike Air Jordan
+            </Shoes>
+          </View>
+
+          <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+            <Shoes img={require('../../assets/3.png')} cost="R$189,90">
+             Adidas Super Star
+            </Shoes>
+            <Shoes img={require('../../assets/4.png')} cost="R$450,00">
+              Adidas Runstatic
+            </Shoes>
+          </View>
+
+          <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+            <Shoes img={require('../../assets/5.png')} cost="R$325,00">
+              Adidas Run
+            </Shoes>
+            <Shoes img={require('../../assets/6.png')} cost="R$230,00">
+              Adidas Indoor
+            </Shoes>
+          </View>
+        </ScrollView>
+          
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    width: '100%',
+    backgroundColor: '#fff'
+  },
+  header:{
+    marginBottom: 8
+  },
+  image:{
+    width: '100%'
+  },
+  textContainer:{
+    flexDirection: 'row',
+    marginVertical: '5%',
+    marginHorizontal: '5%'
+  },
+  text:{
+    fontFamily: 'Anton_400Regular',
+    fontSize: 26,
+    marginHorizontal: '1%'
+  },
+  line:{
+    borderBottomColor: '#D8D8D8',
+    borderBottomWidth: 2
+  }
+});
